c4178bdbf9f0e6dc2ddb26d05181c4698067e289
Johan Melin <soldierheart@hotmail.com>
2021-11-26 04:20:44 +0100

    init heroku

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-let i = 0;
-setInterval(function(){ 
-    console.log("Hello. " + i++)
-}, 3000);
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send("Server has been running for " + process.uptime() + "seconds.")
+})
+
+app.listen(port, () => {
+    console.log("LISTENING ON PORT "+port);
+})
